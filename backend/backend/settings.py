d7ab5fd04eb3a9b63e7475361bdf32cd4493cd2c
Maria Kerimova <xfatcorex@gmail.com>
2024-08-05 08:21:09 +0300

    add taaaski.zapto.org

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.252.142.118', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['84.252.142.118', '127.0.0.1', 'localhost', 'taaaaski.zapto.org']
 
 
 # Application definition
